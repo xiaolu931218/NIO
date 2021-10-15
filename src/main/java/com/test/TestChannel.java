059c9dd4b9e05402309b69396052f186e7cb5df3
xiaolu <1324790234@qq.com>
2021-10-15 22:47:17 +0800

    使用channel读取到buffer

--- src/main/java/com/test/TestChannel.java
+++ src/main/java/com/test/TestChannel.java
@@ -1,8 +1,42 @@
 package com.test;
 
-import java.nio.channels.Channel;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
 
 public class TestChannel {
+
+    /**
+     * 使用channel读取到buffer
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+
+        RandomAccessFile aFile = new RandomAccessFile("F:\\tmp\\aaa.txt", "rw");
+
+        // 创建filechannel
+        FileChannel fileChannel = aFile.getChannel();
+
+        // 创建buffer
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024*1024);
+
+        // 读取数据到buffer中
+        int read = fileChannel.read(byteBuffer);
+        while (read != -1) {
+
+            System.out.println("读取了》》" + read);
+            byteBuffer.flip();
+            while (byteBuffer.hasRemaining()) {
+                System.out.println((char) byteBuffer.get());
+            }
+            byteBuffer.clear();
+            read = fileChannel.read(byteBuffer);
+        }
+        aFile.close();
+        System.out.println("结束了！！！");
+    }
 
 
 }
