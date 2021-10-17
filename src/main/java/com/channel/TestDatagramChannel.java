52e0a9650f296cf2861b7b9005041a112be49f4b
xiaolu <1324790234@qq.com>
2021-10-17 09:18:09 +0800

    TestDatagramChannel

--- src/main/java/com/channel/TestDatagramChannel.java
+++ src/main/java/com/channel/TestDatagramChannel.java
@@ -47,4 +47,32 @@ public class TestDatagramChannel {
             System.out.println(Charset.forName("UTF-8").decode(receiveBuffer));
         }
     }
+    //连接  read  和 write
+    @Test
+    public void testConnect() throws Exception {
+        //打开DatagramChannel
+        DatagramChannel connChannel = DatagramChannel.open();
+        //绑定
+        connChannel.bind(new InetSocketAddress(9999));
+
+        //连接
+        connChannel.connect(new InetSocketAddress("127.0.0.1",9999));
+
+        //write方法
+        connChannel.write(ByteBuffer.wrap("发送atguigu".getBytes("UTF-8")));
+
+        //buffer
+        ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+
+        while(true) {
+
+            readBuffer.clear();
+
+            connChannel.read(readBuffer);
+
+            readBuffer.flip();
+            System.out.println(Charset.forName("UTF-8").decode(readBuffer));
+
+        }
+    }
 }
