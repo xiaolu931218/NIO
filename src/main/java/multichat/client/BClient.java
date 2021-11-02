a696e8dc389889afd50d4568807af5cc28e949a6
xiaolu <1324790234@qq.com>
2021-11-03 00:07:54 +0800

    提交多人聊天室案例代码

---
+++ src/main/java/multichat/client/BClient.java
@@ -0,0 +1,14 @@
+package multichat.client;
+
+import java.io.IOException;
+
+public class BClient {
+
+    public static void main(String[] args) {
+        try {
+            new ChatClient().startClient("mary");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
