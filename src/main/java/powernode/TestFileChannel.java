b998a2ecdf423a33d2e799e20eead8f5c42d98bc
xiaolu <1324790234@qq.com>
2021-11-12 22:12:58 +0800

    RandomAccessFile获得的Channel是双向的

--- src/main/java/powernode/TestFileChannel.java
+++ src/main/java/powernode/TestFileChannel.java
@@ -2,9 +2,7 @@ package powernode;
 
 import org.junit.Test;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.*;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 
@@ -22,5 +20,28 @@ public class TestFileChannel {
         MappedByteBuffer mappedByteBuffer = inChannel.map(FileChannel.MapMode.READ_ONLY, 0, inFile.length());
         FileChannel outChannel = new FileOutputStream(outFile).getChannel();
         outChannel.write(mappedByteBuffer);
+    }
+
+    /**
+     * RandomAccessFile获得的Channel是双向的，
+     * 将out.txt中的内容写到out.txt文件的结尾处
+     */
+    @Test
+    public void test02() {
+
+        File outFile = new File("F:\\tmp\\out.txt");
+        try (
+                FileChannel channel = new RandomAccessFile(outFile,"rw").getChannel();
+        ){
+            // 把Channel中的数据映射到到虚拟内存中
+            MappedByteBuffer buffer = channel.map(FileChannel.MapMode.READ_WRITE, 0, outFile.length());
+            // 设置channel的position属性
+            channel.position(outFile.length());
+            channel.write(buffer);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
